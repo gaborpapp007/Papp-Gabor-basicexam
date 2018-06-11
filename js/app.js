f4efd34865d6b5aae40c78e1352b5897612c0856
Gabor Papp <pappgabesz007@gmail.com>
2018-06-11 11:33:43 +0200

    rendezés + törlés

--- js/app.js
+++ js/app.js
@@ -12,8 +12,36 @@ function getData(url, callbackFunc) {
 function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
-  console.log(userDatas);
 
+
+
+  rendezesArSzerintNovekvo(userDatas);
+  console.log(userDatas);
 }
 
-getData('/json/transport.json', successAjax);
+function rendezesArSzerintNovekvo(tomb) {
+  var csere = 0;
+  var volt_csere = true;
+  while (volt_csere) {
+    volt_csere = false;
+    for (i = 0; i < tomb.length - 1; ++i) {
+      if (tomb[i].cost_in_credits > tomb[i + 1].cost_in_credits) { // ha nagyobb, akkor csere
+        volt_csere = true;
+        csere = tomb[i];
+        tomb[i] = tomb[i + 1];
+        tomb[i + 1] = csere;
+      }
+    }
+  }
+  return tomb;
+}
+
+function torlesAholNull(tomb) {
+  for (var i = 0; i < array.length; i++) {
+    if (tomb[i].consumables == null) {
+      tomb.splice(i, 1);
+    }
+  }
+  return tomb;
+}
+getData('/json/spaceships.json', successAjax);
